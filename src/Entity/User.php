9e2289fce42738f43689b39e8cc857ea1b1b929e
Phostriche <christophe@cidees.net>
2022-02-18 10:00:56 +0100

    Little refactoring, use string instead of array for email unique entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,6 +10,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
@@ -29,16 +30,20 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
 
       /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Please enter your first name")
      */
     private $firstName;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Please enter your last name")
      */
     private $lastName;
 
     /**
      * @ORM\Column(type="string", length=180, unique=true)
+     * @Assert\NotBlank(message="Please enter your email adress")
+     * @Assert\Email(message="Please enter a valid email adress")
      */
     private $email;
 
